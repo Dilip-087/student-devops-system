e355f2e6c24b374596f70603df26eba1c637e3c2
Dilip-087 <dilip.b2023@vitstudent.ac.in>
2026-09-17 09:00:09 +0530

    Create student performance report in app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+with open("student_report.txt", "w") as f:
+    f.write("=== STUDENT PERFORMANCE SYSTEM REPORT ===\n")
+    f.write("Total Registered Students: 120\n")
+    f.write("Average Batch Marks: 84%\n")
+print("Report document successfully created.")
